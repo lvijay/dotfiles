4ac7280dd7b80758a1499a59f2af1b604132c2b2
Vijay Lakshminarayanan <laksvij@gmail.com>
2011-08-26 14:18:00 +0530

    added emacs configuration files

---
+++ .emacs.d/elpa/cygwin-mount-2001/cygwin-mount-autoloads.el
@@ -0,0 +1,22 @@
+;;; cygwin-mount-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("cygwin-mount-pkg.el" "cygwin-mount.el")
+;;;;;;  (20017 17064 757000))
+
+(load "cygwin-mount")
+
+(cygwin-mount-activate)
+
+;;;***
+
+(provide 'cygwin-mount-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; cygwin-mount-autoloads.el ends here
