df5466cf9195001c20caa1c30ffeebd602768947
Vijay Lakshminarayanan <laksvij@gmail.com>
2019-11-02 08:38:53 +0530

    169

--- code/other-stuff/codingame/chucknorris.py
+++ code/other-stuff/codingame/chucknorris.py
@@ -1,4 +1,4 @@
-a,z,j='X0X'
+a,z='X0'
 s=''.join([bin(0x80|ord(c))[3:]for c in input()])
 while s:f=s[0];j=s.find('01'[f==z]);j=[len(s),j][j>=0];s=s[j:];a+='%s %s '%(z+['',z][f==z],z*j)
 print(a[1:-1])
