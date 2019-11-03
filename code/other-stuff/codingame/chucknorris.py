a61205cccca1fcfc0ee996418b23f4a9d7e420d1
Vijay Lakshminarayanan <laksvij@gmail.com>
2019-11-03 14:41:35 +0530

    chucknorris 110

--- code/other-stuff/codingame/chucknorris.py
+++ code/other-stuff/codingame/chucknorris.py
@@ -1,4 +1,4 @@
 z,a,f='0 _'
 for c in input():
- for v in bin(128|ord(c))[3:]:a+=[f' {z+z*[v==z]} ',''][f==v]+z;f=v
+ for v in bin(128|ord(c))[3:]:a+=[f' {z+z*(v==z)} ',''][f==v]+z;f=v
 print(a[2:])
