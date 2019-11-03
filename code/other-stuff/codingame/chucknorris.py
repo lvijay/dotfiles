2b496ba04a86e0f43ab7dcd0006c9197f51f1a80
Vijay Lakshminarayanan <laksvij@gmail.com>
2019-11-03 21:22:56 +0530

    intermediate experiment with string replace

--- code/other-stuff/codingame/chucknorris.py
+++ code/other-stuff/codingame/chucknorris.py
@@ -1,4 +1,15 @@
-z,a,f='0**'
-for c in input():
- for v in bin(128|ord(c))[3:]:a+=f' {z+z*(v==z)} '*(f!=v)+z;f=v
-print(a[2:])
+#z,a,f='0**'
+#for c in input():
+# for v in bin(128|ord(c))[3:]:a+=f' {z+z*(z==v)} '*(f!=v)+z;f=v
+#print(a[2:])
+
+r=str.replace
+r(r(r(r(''.join([bin(128|ord(c))[3:]for c in input()]),'01','0x0'),'10','0y0'),'x',' 0 '),'y',' 00 ')
+('xy'[q[0]=='0']+q[1:].replace('01','0x1').replace('10','0y0')).replace('x',' 0 ').replace('y',' 00 ').replace('1','0')
+def dwim(s):
+ z,a,f='0**'
+ print(''.join([bin(128|ord(c))[3:]for c in s]))
+ for c in s:
+  for v in bin(128|ord(c))[3:]:
+   a+=f' {z+z*(v==z)} '*(f!=v)+z;f=v
+ return(a[2:])
