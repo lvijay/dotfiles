7e32e5385261203b82ce402d66a42f172b07ead7
Vijay Lakshminarayanan <laksvij@gmail.com>
2019-11-03 12:13:27 +0530

    160

--- code/other-stuff/codingame/chucknorris.py
+++ code/other-stuff/codingame/chucknorris.py
@@ -1,4 +1,4 @@
 a,z='X0'
 s=''.join([bin(0x80|ord(c))[3:]for c in input()])
-while s:f=s[0]==z;j=s.find('01'[f]);j=[len(s),j][j>=0];s=s[j:];a+=' %s %s'%([z,z+z][f],z*j)
+while s:f=s[0]==z;j=s.find('01'[f]);j=[len(s),j][j>=0];s=s[j:];a+=f' {[z,z+z][f]} {z*j}'
 print(a[2:])
