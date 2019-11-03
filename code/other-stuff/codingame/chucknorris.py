ac845e9836466f3622229db7a0ded9aa5b699c98
Vijay Lakshminarayanan <laksvij@gmail.com>
2019-11-03 12:41:55 +0530

    chucknorris 132

--- code/other-stuff/codingame/chucknorris.py
+++ code/other-stuff/codingame/chucknorris.py
@@ -1,7 +1,5 @@
-z='0'
-s=''.join([bin(0x80|ord(c))[3:]for c in input()])
-f=s[0];a=f'{[z,z+z][f==z]} '
+z,s,a,*f='0',''.join([bin(0x80|ord(c))[3:]for c in input()]),''
 for v in s:
  if f!=v:a,f=a+f' {[z,z+z][v==z]} ',v
  a+=z
-print(a)
+print(a[1:])
