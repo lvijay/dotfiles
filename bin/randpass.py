605bc256cc29f75ee137ceb86d72ea0253b04f19
Vijay Lakshminarayanan <vijay.lakshminarayanan@freshworks.com>
2021-05-13 05:13:50 +0530

    bumps up to python3 randpass.py, randstring.py

--- bin/randpass.py
+++ bin/randpass.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 WORDFILE='/usr/share/dict/words'
 
 def randstring(r, n, data):
     lim = len(data)
-    return ' '.join([data[r.randint(0, lim)] for i in xrange(n)])
+    return ' '.join([data[r.randint(0, lim)] for i in range(n)])
 
 def main(argv):
     from random import SystemRandom
 
     if len(argv) == 1:
-        print >> sys.stderr, 'Usage %s n' % argv[0]
+        print('Usage %s n' % argv[0], file=sys.stderr)
         sys.exit(1)
         return
 
@@ -18,11 +18,11 @@ def main(argv):
         n = int(argv[1])
         with open(WORDFILE, 'r') as instream:
             data = [line.strip() for line in instream.readlines() if len(line) > 4]
-            print randstring(SystemRandom(), n, data)
+            print(randstring(SystemRandom(), n, data))
     except ValueError:
-        print >> sys.stderr, "Expected number got '%s'" % argv[1]
+        print(f"Expected number got '{argv[1]}'", file=sys.stderr)
         sys.exit(1)
-        return
+        return                  # unreachable
     
 if __name__ == '__main__':
     import sys
--- bin/randstring.py
+++ bin/randstring.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ALPHANUMERICS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
@@ -7,24 +7,24 @@ PRINTABLE_ASCII = ALPHANUMERICS + ALPHANUMERICS + ALPHANUMERICS + '!"#$%&\'()*+,
 
 def randstring(r, n):
     lim = len(PRINTABLE_ASCII) - 1
-    return ''.join([PRINTABLE_ASCII[r.randint(0, lim)] for i in xrange(n)])
+    return ''.join([PRINTABLE_ASCII[r.randint(0, lim)] for i in range(n)])
 
 def main(argv):
     from random import SystemRandom
 
     if len(argv) == 1:
-        print >> sys.stderr, 'Usage %s n' % argv[0]
+        print('Usage %s n' % argv[0], file=sys.stderr)
         sys.exit(1)
         return
 
     try:
         n = int(argv[1])
     except ValueError:
-        print >> sys.stderr, "Expected number got '%s'" % argv[1]
-        sys.exit(1)
+        print("Expected number got '%s'" % argv[1], file=sys.stderr)
+        sys.exit(2)
         return
 
-    print randstring(SystemRandom(), n)
+    print(randstring(SystemRandom(), n))
 
 if __name__ == '__main__':
     import sys
